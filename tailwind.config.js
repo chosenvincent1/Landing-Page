9bab917ec41d389b682654ec0614560d06539b50
Chosen <chosendev2000@gmail.com>
2023-05-06 22:19:22 +0100

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './public/**/*.html',
+    './src/**/*.js',
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
